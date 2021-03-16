914539627cb6341bfe115691bb17c081e7a415f6
Jacob Reimers <jacob@reimers.dk>
2021-03-16 16:53:36 +0100

    Create test for certificate with SAN for IP address.

---
+++ tests/Reimers.Ihe.Communication.Tests/SecureIheTransactionWithSanTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Reimers.Ihe.Communication.Tests
+{
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Threading.Tasks;
+    using NHapi.Model.V251.Message;
+    using Xunit;
+
+    public class SecureIheTransactionWithSanTests : IDisposable
+    {
+        private readonly MllpServer _server;
+        private readonly int _port = 2576;
+        private readonly X509Certificate2Collection _cert;
+
+        public SecureIheTransactionWithSanTests()
+        {
+            var certificate = X509Certificate2.CreateFromPemFile("cert.pem", "san.pem");
+            var buffer = certificate.Export(X509ContentType.Pfx, (string)null);
+            certificate = new X509Certificate2(buffer, (string)null);
+            _cert = new X509Certificate2Collection(
+                certificate);
+            _server = new MllpServer(
+                new IPEndPoint(IPAddress.Loopback, _port),
+                NullLog.Get(),
+                new TestMiddleware(),
+                serverCertificate: _cert[0],
+                userCertificateValidationCallback: UserCertificateValidationCallback);
+            _server.Start();
+        }
+
+        private static bool UserCertificateValidationCallback(
+            object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            return sslPolicyErrors == SslPolicyErrors.None || sslPolicyErrors == SslPolicyErrors.RemoteCertificateChainErrors;
+        }
+
+        [Fact]
+        public async Task WhenSendingMessageThenGetsAck()
+        {
+            var client = await MllpClient.Create(
+                IPAddress.Loopback.ToString(),
+                _port,
+                clientCertificates: _cert,
+                userCertificateValidationCallback:
+                UserCertificateValidationCallback).ConfigureAwait(false);
+            var request = new QBP_Q11();
+            request.MSH.MessageControlID.Value = "test";
+            var response = await client.Send(request).ConfigureAwait(false);
+            Assert.NotNull(response);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            _server?.DisposeAsync().AsTask().Wait();
+        }
+    }
+}
